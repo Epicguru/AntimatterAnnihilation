3940e986de1b93ad6dbb4eb420921bfc2c4126b3
James Billy <james@epicguru.co.uk>
2020-05-11 20:38:56 +0200

    Added composite, groundwork for in-game wiki added.

---
+++ Source/InGameWiki/WikiElement.cs
@@ -0,0 +1,61 @@
+﻿using UnityEngine;
+using Verse;
+
+namespace InGameWiki
+{
+    public class WikiElement
+    {
+        public string Text;
+
+        public Texture2D Image;
+        public Vector2 ImageSize = new Vector2(-1, -1);
+        public float ImageScale = 1f;
+
+        public WikiPage Link;
+
+        public bool HasText
+        {
+            get
+            {
+                return !string.IsNullOrWhiteSpace(Text);
+            }
+        }
+
+        public bool HasImage
+        {
+            get
+            {
+                return Image != null;
+            }
+        }
+
+        public virtual Vector2 Draw(Rect maxBounds)
+        {
+            Vector2 size = Vector2.zero;
+
+            if (HasImage)
+            {
+                float width = ImageSize.x < 1 ? Image.width * ImageScale : ImageSize.x;
+                float height = ImageSize.y < 1 ? Image.height * ImageScale : ImageSize.y;
+                Widgets.DrawTextureFitted(new Rect(maxBounds.x, maxBounds.y, width, height), Image, 1f);
+                size += new Vector2(width, height);
+            }
+
+            if (HasText)
+            {
+                float x = maxBounds.x;
+                float width = maxBounds.xMax - x;
+                //Widgets.Label(new Rect(x, maxBounds.y, width, maxBounds.height), Text);
+
+                float startY = maxBounds.y;
+                float cacheStartY = startY;
+                Widgets.LongLabel(x, width, Text, ref startY);
+                float change = cacheStartY - startY;
+
+                size += new Vector2(width, change);
+            }
+
+            return size;
+        }
+    }
+}
---
+++ Source/InGameWiki/WikiPage.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+using Verse;
+
+namespace InGameWiki
+{
+    public class WikiPage
+    {
+        public string Title;
+        public string ShortDescription;
+        public Texture2D Icon;
+
+        public List<WikiElement> Elements = new List<WikiElement>();
+
+        public virtual void Draw(Rect bounds)
+        {
+            // TODO draw title, icon, description.
+
+            // Draw elements.
+            // TODO scroll.
+            foreach (var page in Elements)
+            {
+                if (page == null)
+                    return;
+
+                Rect pos = bounds;
+                try
+                {
+                    var size = page.Draw(pos);
+                    pos.y += size.y + 10;
+                }
+                catch (Exception e)
+                {
+                    Log.Error($"In-game wiki exception when drawing element: {e}");
+                }
+            }
+        }
+    }
+}
