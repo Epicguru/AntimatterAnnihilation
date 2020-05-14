02d3eb2c91d201c27300eb5663b1c19d8013a8d7
James Billy <james@epicguru.co.uk>
2020-05-14 22:13:04 +0200

    Added resonating blade, galvoknuckles, secret sword.

---
+++ Source/AntimatterAnnihilation/Damage/DamageWorker_GalvaPain.cs
@@ -0,0 +1,12 @@
+﻿using Verse;
+
+namespace AntimatterAnnihilation.Damage
+{
+    public class DamageWorker_GalvaPain : DamageWorker_AddInjury
+    {
+        protected override BodyPartRecord ChooseHitPart(DamageInfo dinfo, Pawn pawn)
+        {
+            return pawn.health.hediffSet.GetRandomNotMissingPart(dinfo.Def, dinfo.Height, BodyPartDepth.Outside, null);
+        }
+    }
+}
