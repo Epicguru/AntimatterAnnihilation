780392980cfca8a1f3f83487cfa6421f0abc0896
James Billy <james@epicguru.co.uk>
2020-05-21 23:43:16 +0200

    Added temporary reference to in-game wiki mod, which has moved.

--- Source/InGameWiki/CompoundWikiElement.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Collections.Generic;
-
-namespace InGameWiki
-{
-    public abstract class CompoundWikiElement : WikiElement
-    {
-        public List<WikiElement> Elements = new List<WikiElement>();
-    }
-}
