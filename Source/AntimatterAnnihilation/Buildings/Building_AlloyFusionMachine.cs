dda91f94f527b7058f4b425a20c69b3246799881
James Billy <james@epicguru.co.uk>
2020-05-25 17:25:28 +0200

    Added all sprites, xml. WIP.

---
+++ Source/AntimatterAnnihilation/Buildings/Building_AlloyFusionMachine.cs
@@ -0,0 +1,18 @@
+﻿using RimWorld;
+
+namespace AntimatterAnnihilation.Buildings
+{
+    public class Building_AlloyFusionMachine : Building_Storage
+    {
+        public CompPowerTrader PowerTraderComp
+        {
+            get
+            {
+                if (_powerTraderComp == null)
+                    this._powerTraderComp = base.GetComp<CompPowerTrader>();
+                return _powerTraderComp;
+            }
+        }
+        private CompPowerTrader _powerTraderComp;
+    }
+}
