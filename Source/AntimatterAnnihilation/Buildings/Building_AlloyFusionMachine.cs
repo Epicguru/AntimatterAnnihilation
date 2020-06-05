786494917aec8ccdd31806679f7bb3a54671984c
James Billy <james@epicguru.co.uk>
2020-06-05 20:21:03 +0200

    Updated to newer in-game wiki version for debugging.

--- Source/AntimatterAnnihilation/Buildings/Building_AlloyFusionMachine.cs
+++ Source/AntimatterAnnihilation/Buildings/Building_AlloyFusionMachine.cs
@@ -1,8 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using AntimatterAnnihilation.ThingComps;
+﻿using AntimatterAnnihilation.ThingComps;
 using AntimatterAnnihilation.Utils;
 using RimWorld;
+using System.Collections.Generic;
 using UnityEngine;
 using Verse;
 
